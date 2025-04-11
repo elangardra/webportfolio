dc72c5afe2014877c9a19653364275bb645b4c60
elangardra <elangardra12@gmail.com>
2025-04-11 16:10:16 +0700

    Fix Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        outDir: 'public/build',
+        assetsDir: '',
+        manifest: true,
+    },
 });
